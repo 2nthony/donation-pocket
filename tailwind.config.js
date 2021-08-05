771ebe58cd29f4bd093f3254d207761fbfe325d2
evillt <ijoec123@gmail.com>
2021-08-05 21:35:03 +0800

    -screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '-2xl': { max: '1535px' },
+        '-xl': { max: '1279px' },
+        '-lg': { max: '1023px' },
+        '-md': { max: '767px' },
+        '-sm': { max: '639px' },
+      },
+    },
   },
   variants: {
     extend: {},
